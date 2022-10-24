b818de78ac823131f1215e008d6df993b01a7dc5
Junaid Ejaz <junaidejaz16@gmail.com>
2022-10-24 11:30:01 +0100

    DbContext - setup

---
+++ Project/NzWalk/Data/NZWalkDbCtx.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalk.Model.Domain;
+
+namespace NzWalk.Data
+{
+    public class NZWalkDbCtx : DbContext
+    {
+        public NZWalkDbCtx(DbContextOptions<NZWalkDbCtx> options) : base(options)
+        {
+        }
+        //Models 
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ Project/NzWalk/Model/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NzWalk.Model.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
